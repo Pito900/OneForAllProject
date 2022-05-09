0291598eaf23c1d53185f6b392a481e0f8272a50
Francisco Monteiro <franciscorubenilto@gmail.com>
2022-05-09 17:23:27 -0300

    desafio 4 - ajustando

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,9 @@
 SELECT
   u.usuario AS usuario,
-  IF(MAX(YEAR(h.data_reproducao)) >= 2021, 'usuario ativo', 'usuario inativo') AS condicao_usuario
+  IF(MAX(YEAR(h.data_reproducao)) >= 2021, 'Usuário ativo', 'Usuário inativo') AS condicao_usuario
 FROM SpotifyClone.usuarios AS u
 INNER JOIN SpotifyClone.historico_reproducao AS h ON u.usuario_id = h.usuario_id
 GROUP BY
   usuario
 ORDER BY
   usuario;
-  
